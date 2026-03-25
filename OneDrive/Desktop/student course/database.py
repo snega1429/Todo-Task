a7e0365b7a86171260cb8b8e51723df427a7104a
Snega <snega14102001@gmail.com>
2026-03-25 12:33:02 +0530

    Remove unnecessary folders and files

--- OneDrive/Desktop/student course/database.py
+++
@@ -1,9 +0,0 @@
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
-from models import Base
-
-DATABASE_URL = "postgresql://postgres:2004@localhost/task_db"
-
-engine = create_engine(DATABASE_URL)
-SessionLocal = sessionmaker(bind=engine)
-Base.metadata.create_all(bind=engine)
--- Todo Task/frontend/src/api/api.tsx
+++
@@ -1,9 +0,0 @@
-import axios from "axios";
-
-const api = axios.create({
-  baseURL: "https://dashboard.render.com/d/dpg-d71a73ea2pns73f1np4g-a"
-});
-
-
-
-export default api;
