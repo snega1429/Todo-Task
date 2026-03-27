45745d1d32b1014c554413e14eb7f7bbfe145b39
Snega <snega14102001@gmail.com>
2026-03-27 14:22:26 +0530

    changes

--- backend/database.py
+++ backend/database.py
@@ -4,6 +4,8 @@ from dotenv import load_dotenv
 import os
 
 load_dotenv()
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+env_path = os.path.join(BASE_DIR, ".env")
 
 APP_URL = os.getenv("APP_URL")
 
